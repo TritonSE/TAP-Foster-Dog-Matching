d53e5a96709c08647a608e1ac0572ea3db31ac35
Andrew Masek <andrewmasek@gmail.com>
2022-01-16 00:39:42 -0800

    Populated dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Header from "../components/Header";
+import DefaultBody from "../components/DefaultBody";
+import DashboardCard from "../components/DashboardCard";
+import IconButton from "../components/IconButton";
+import dogCollage from "../images/dogcollage2.png";
+import plus from "../images/plus.png";
 
 function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Header firstName="Placeholder" />
+      <DefaultBody>
+        <DashboardCard
+          imagePath={dogCollage}
+          imageAltText="Dog decoration image"
+          cardText="Start Your Application"
+          // TODO Give IconButton link href so it can function
+          iconButton={
+            <IconButton icon={plus} altText="ContinueButton" leftOffset="304px" topOffset="156px" />
+          }
+        />
+      </DefaultBody>
     </div>
   );
 }
