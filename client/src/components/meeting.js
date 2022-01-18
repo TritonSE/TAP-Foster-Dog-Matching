b265fcb23a814835905c71ce3a1dd3bbd1a2f977
Jacob Au <jacobau.inbox@gmail.com>
2022-01-18 15:29:21 -0800

    fixed lint

--- client/src/components/meeting.js
+++ client/src/components/meeting.js
@@ -2,20 +2,16 @@ import React from "react";
 import "../styles/meeting.css";
 
 function Meetings(props) {
-    return (
-      <div className="meeting-card">
-        <div className="titleContainer">
-            <h3 className="title">{props.title}</h3>
-        </div>
-        <div className="break"></div>
-        <div className="text-card">
-            {props.textCard}
-        </div>
-        <div className="image-card  ">
-            <img className="image" src={props.imagePath} alt="not found"></img>
-        </div>
+  return (
+    <div className="meeting-card">
+      <div className="title">{props.title}</div>
+      <div className="break" />
+      <div className="text-card">{props.textCard}</div>
+      <div className="image-card  ">
+        <img className="image" src={props.imagePath} alt="not found" />
       </div>
-    );
-  }
-  
-  export default Meetings;
+    </div>
+  );
+}
+
+export default Meetings;
--- client/src/pages/InitialInterview.js
+++ client/src/pages/InitialInterview.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Meeting from "../components/meeting";
-import MeetingsText from "../components/meeting-text";
-import Doggo from "../images/good-boi.png"
+import Doggo from "../images/good-boi.png";
 
 function InitialInterview() {
-    return (
-        <div>
-            <h1>Initial Interview</h1>
-            <Meeting 
-                title="Interview Confirmed"
-                imagePath={Doggo}
-                textCard = {
-                    <div>
-                        <p>Hello Bob</p>
-                        <p>Congratulations on confirming your first interview. That is pogpilled and based.</p>
-                        <p>The Animal Pad Team</p>
-                    </div>
-                }
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Initial Interview</h1>
+      <Meeting
+        title="Interview Confirmed"
+        imagePath={Doggo}
+        textCard={
+          <div>
+            <p>Hello Bob</p>
+            <p>Congratulations on confirming your first interview. That is pogpilled and based.</p>
+            <p>The Animal Pad Team</p>
+          </div>
+        }
+      />
+    </div>
+  );
 }
 
 export default InitialInterview;
