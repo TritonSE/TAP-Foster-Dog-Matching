5104238100764eebf7a952b3af4cb2973ce0a986
Artyom <artyom.muradyan2001@gmail.com>
2022-01-18 18:30:38 -0800

    basic buggy styling

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,34 +5,48 @@ import dog from "../images/dog.png";
 import "../styles/landing.css";
 
 function Landing() {
-  const foster = {
+  const fosterBorder = {
     position: "absolute",
     width: "182px",
     height: "69px",
     top: "182px",
     left: "570px",
   };
-  const volunteer = {
+  const volunteerBorder = {
     position: "absolute",
     width: "182px",
     height: "69px",
     top: "467px",
     left: "570px",
   };
+  const fosterText = {
+    position: "static",
+    width: "62px",
+    height: "29px",
+    left: "60px",
+    top: "20px",
+  };
+  const volunteerText = {
+    position: "static",
+    width: "62px",
+    height: "29px",
+    left: "67px",
+    top: "20px",
+  };
 
   return (
     <div>
       <Header />
-      <div id="landing-body">
-        <div className="form-container">
+      <div>
+        <div className="form-login-container">
           <b className="form-header">Welcome</b>
-          <div>Are you a...</div>
-          <Button styleBorder={foster} name="Foster" />
-          <Button styleBorder={volunteer} name="Volunteer" />
+          <div id="form-landing-question">Are you a...</div>
+          <Button styleBorder={fosterBorder} styleText={fosterText} name="Foster" />
+          <Button styleBorder={volunteerBorder} styleText={volunteerText} name="Volunteer" />
         </div>
-        <div id="landing-right-panel">
-          <img id="landing-dog" src={dog} alt="Cute dog!" />
-          <div id="copyright">
+        <div className="landing-right-panel">
+          <img className="dog-image" src={dog} alt="Cute dog!" />
+          <div className="copyright">
             © 2020 The Animal Pad | All rights reserved The Animal Pad is a 501c3 Organization EIN
             #45-4902841.
           </div>
