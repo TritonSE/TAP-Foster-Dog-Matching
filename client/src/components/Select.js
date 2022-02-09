5b390b032a22ac9e99b263da966c7595aec5d6cd
parth4apple <worldrecord00@gmail.com>
2022-02-08 19:51:07 -0800

    lint fix

--- client/src/components/Select.js
+++ client/src/components/Select.js
@@ -31,7 +31,6 @@ const StyledSelect = styled.div`
   justify-content: space-between;
   position: relative;
   cursor: pointer;
-  background-color: #C6C6C6;
   z-index: ${(props) => (props.open ? 3 : 1)};
 `;
 
