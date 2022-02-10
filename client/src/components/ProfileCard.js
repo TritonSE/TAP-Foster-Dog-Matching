6e3973a7846aa4b9d8ef83c33c4f079ce56c6f3d
William Jin <wjin0702@yahoo.com>
2022-02-09 22:19:23 -0800

    Update mobile css for profile page

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -4,10 +4,14 @@ import "../styles/profilecard.css";
 function ProfileCard(props) {
   return (
     <div className="profile-card">
-      <img className="profile-card-image" src={props.imagePath} alt={props.imageAltText} />
-      <div className="profile-card-name">{props.name}</div>
-      <div className="profile-card-phonenumber">{props.phonenumber}</div>
-      <div className="profile-card-email">{props.email}</div>
+      <div className="profile-card-image-container">
+        <img className="profile-card-image" src={props.imagePath} alt={props.imageAltText} />
+      </div>
+      <div className="profile-card-text">
+        <div className="profile-card-name">{props.name}</div>
+        <div className="profile-card-contact-info">{props.phonenumber}</div>{" "}
+        <div className="profile-card-contact-info">{props.email}</div>
+      </div>
     </div>
   );
 }
