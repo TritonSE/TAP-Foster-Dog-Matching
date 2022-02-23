e444fd44f89c552c0211c3cba90130a4b75a0869
Philip Zhang <code@philipzha.ng>
2022-02-22 17:34:47 -0800

    form field names

--- client/src/pages/Application.js
+++ client/src/pages/Application.js
@@ -29,7 +29,7 @@ function FosterApplication({ setView }) {
 
   const onError = (errors) => {
     console.log(errors);
-    setView("agreement");
+    // setView("agreement");
   };
 
   return (
@@ -38,8 +38,8 @@ function FosterApplication({ setView }) {
         <Form.Title>Foster Application</Form.Title>
         <Form.Section title="Personal Information" ref={personalInfoRef}>
           <Form.SubSection title="Name">
-            <ControlledInput control={control} label="First Name" name="firstname" required />
-            <ControlledInput control={control} label="Last Name" name="lastname" required />
+            <ControlledInput control={control} label="First Name" name="firstName" required />
+            <ControlledInput control={control} label="Last Name" name="lastName" required />
           </Form.SubSection>
           <Form.SubSection title="Address">
             <ControlledInput
@@ -58,7 +58,7 @@ function FosterApplication({ setView }) {
               <ControlledInput control={control} label="State" name="address.state" required />
             </Form.Row>
             <Form.Row>
-              <ControlledInput control={control} label="Zipcode" name="address.zipCode" required />
+              <ControlledInput control={control} label="Zipcode" name="address.zipcode" required />
               <ControlledInput control={control} label="Country" name="address.country" required />
             </Form.Row>
           </Form.SubSection>
@@ -75,17 +75,38 @@ function FosterApplication({ setView }) {
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>Do you own or rent your home?</InputLabel>
+            {/* TODO: radios name="homeType" required  */}
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>Landlord&apos;s Name (if you own, just write n/a)*</InputLabel>
             <Form.Row>
               <Form.Column>
-                <ControlledInput control={control} label="First Name" />
-                <ControlledInput control={control} label="Landlord's Phone Number" />
+                <ControlledInput
+                  control={control}
+                  label="First Name"
+                  name="landlord.firstName"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Landlord's Phone Number"
+                  name="landlord.phone"
+                  required
+                />
               </Form.Column>
               <Form.Column>
-                <ControlledInput control={control} label="Last Name" />
-                <ControlledInput control={control} label="Landlord's Email Address" />
+                <ControlledInput
+                  control={control}
+                  label="Last Name"
+                  name="landlord.lastName"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Landlord's Email Address"
+                  name="landlord.email"
+                  required
+                />
               </Form.Column>
             </Form.Row>
           </Form.SubSection>
@@ -97,25 +118,41 @@ function FosterApplication({ setView }) {
               control={control}
               label="Do you have any breed and/or size restrictions where you live. If yes, please list."
               numLines={6}
+              name="fosterInfo.restrictions"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
-            <ControlledInput control={control} label="Where will your foster dog sleep at night?" />
+            <ControlledInput
+              control={control}
+              label="Where will your foster dog sleep at night?"
+              name="fosterInfo.sleepLocation"
+              required
+            />
           </Form.SubSection>
           <Form.SubSection>
-            <ControlledInput control={control} label="How many hours are you home a day?" />
+            <ControlledInput
+              control={control}
+              label="How many hours are you home a day?"
+              name="fosterInfo.hoursAtHome"
+              required
+            />
           </Form.SubSection>
           <Form.SubSection>
             <ControlledInput
               control={control}
               label="Please describe a typical day for your dog while you are away."
               numLines={6}
+              name="fosterInfo.typicalDay"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
             <ControlledInput
               control={control}
               label="Who will be the primary caregiver for this foster dog?"
+              name="fosterInfo.primaryCaregiver"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
@@ -123,17 +160,19 @@ function FosterApplication({ setView }) {
               If you reside with other adults in the household, are they onboard to bring in a
               foster dog?
             </InputLabel>
-            {/* TODO: radios */}
+            {/* TODO: radios name="fosterInfo.othersOnboard" required*/}
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>Do we have permission to do a home visit?</InputLabel>
-            {/* TODO: radios */}
+            {/* TODO: radios name="fosterInfo.permissionToVisit" required*/}
           </Form.SubSection>
           <Form.SubSection>
             <ControlledInput
               control={control}
               label="Have you ever fostered a dog before? If so, please describe the experience and include as much detail as possible."
               numLines={14}
+              name="fosterInfo.pastExperience"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
@@ -141,6 +180,8 @@ function FosterApplication({ setView }) {
               control={control}
               label="Why do you wish to foster a dog?"
               numLines={14}
+              name="fosterInfo.whyFoster"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
@@ -150,19 +191,20 @@ function FosterApplication({ setView }) {
               and 2 DHPP vaccines for puppies under 4 months. Are you able to make at least a 1
               month commitment to care for your foster dog?*
             </InputLabel>
-            {/* TODO: radios */}
+            {/* TODO: radios name="fosterInfo.oneMonthCommitment"
+              required */}
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>
               Size of Dog you are willing to foster (please mark all that apply): *
             </InputLabel>
-            {/* TODO: checkboxes */}
+            {/* TODO: checkboxes name="fosterInfo.sizeOfDog" required*/}
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>
               Age of Dog you are willing to foster (please mark all that apply): *
             </InputLabel>
-            {/* TODO: checkboxes */}
+            {/* TODO: checkboxes name="fosterInfo.ageOfDog" required */}
           </Form.SubSection>
         </Form.Section>
 
@@ -171,25 +213,64 @@ function FosterApplication({ setView }) {
             <InputLabel>Please provide a personal reference </InputLabel>
             <Form.Row>
               <Form.Column>
-                <ControlledInput control={control} label="First Name" />
-                <ControlledInput control={control} label="Reference's Phone Number" />
-                <ControlledInput control={control} label="Relation" />
+                <ControlledInput
+                  control={control}
+                  label="First Name"
+                  name="reference.firstName"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Reference's Phone Number"
+                  name="reference.phone"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Relation"
+                  name="reference.relation"
+                  required
+                />
               </Form.Column>
               <Form.Column>
-                <ControlledInput control={control} label="Last Name" />
-                <ControlledInput control={control} label="Reference's Email Address" />
-                <ControlledInput control={control} label="Years Known" type="number" />
+                <ControlledInput
+                  control={control}
+                  label="Last Name"
+                  name="reference.lastName"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Reference's Email Address"
+                  name="reference.email"
+                  required
+                />
+                <ControlledInput
+                  control={control}
+                  label="Years Known"
+                  type="number"
+                  name="reference.yearsKnown"
+                  required
+                />
               </Form.Column>
             </Form.Row>
           </Form.SubSection>
           <Form.SubSection>
-            <ControlledInput control={control} label="How did you hear about TAP?" numLines={6} />
+            <ControlledInput
+              control={control}
+              label="How did you hear about TAP?"
+              numLines={6}
+              name="otherInfo.howDidYouHearAboutTAP"
+              required
+            />
           </Form.SubSection>
           <Form.SubSection>
             <ControlledInput
               control={control}
               label="If you have any other pets, please list the number, age, types, and any issues they may have with a new dog."
               numLines={10}
+              name="otherInfo.otherPets"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
@@ -197,17 +278,21 @@ function FosterApplication({ setView }) {
               control={control}
               label="Are all the dogs in your home up to date on vaccines and in good health?"
               numLines={10}
+              name="otherInfo.dogsHealth"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
             <InputLabel>If you have other dogs in the home, are they spayed/neutered?</InputLabel>
-            {/* TODO: radios */}
+            {/* TODO: radios name="otherInfo.dogsNeutered" required*/}
           </Form.SubSection>
           <Form.SubSection>
             <ControlledInput
               control={control}
               label="Do you have any children? If so, please list the number and their experience with dogs."
               numLines={10}
+              name="otherInfo.children"
+              required
             />
           </Form.SubSection>
           <Form.SubSection>
@@ -215,6 +300,8 @@ function FosterApplication({ setView }) {
               control={control}
               label="Please describe your living situation. If you rent, please tell us about your arrangement with your landlord pertaining to dogs."
               numLines={10}
+              name="otherInfo.livingSituation"
+              required
             />
           </Form.SubSection>
         </Form.Section>
