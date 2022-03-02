1e5a83f1bee3a97a745a7ce92f933dd0d1cd1644
parth4apple <worldrecord00@gmail.com>
2022-03-01 17:58:28 -0800

    pr comment fixes

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -20,8 +20,9 @@ function ContactUs() {
   const [inputMessage, setInputMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [sent, setSent] = useState(false);
-  const noRecipient = "Please select a team to send the message to.";
+  const noRecipient = "Please select a team to contact.";
   const emptyText = "Message body cannot be blank.";
+  const [value, setValue] = React.useState("");
 
   const toSelect = [
     { label: "General", value: "General" },
@@ -31,20 +32,23 @@ function ContactUs() {
   ];
 
   const continueDecide = () => {
-    setContinued(true);
-    setSent(false);
+    // Only continue if department is selected
+    if (value === "") {
+      setErrorMessage(noRecipient);
+    } else {
+      setContinued(true);
+      setSent(false);
+      setInputMessage("");
+      setErrorMessage("");
+    }
   };
-
-  const [value, setValue] = React.useState("");
 
   const handleSelect = React.useCallback((val) => {
     setValue(val);
   }, []);
 
   const sendMessage = () => {
-    if (value === "") {
-      setErrorMessage(noRecipient);
-    } else if (inputMessage === "") {
+    if (inputMessage === "") {
       setErrorMessage(emptyText);
     } else {
       setErrorMessage("");
@@ -61,68 +65,84 @@ function ContactUs() {
           "Contact Us": "/contact",
         }}
       />
-      {!continued ? (
-        <div className="contactLanding">
-          <p className="helpQuestion">What do you need help on?</p>
-
-          <div className="selection">
-            <Select
-              value={value}
-              options={toSelect}
-              onChange={handleSelect}
-              placeholder="Select your option"
-              height="4vh"
-              backgroundColor="#C6C6C6"
-            />
-          </div>
-          <button type="button" className="continueButton" onClick={() => continueDecide()}>
-            Continue
-          </button>
-        </div>
-      ) : (
-        <div className="newMessage">
-          <div className="topBar">
-            <p>New Message</p>
-            <button type="button" className="cancel" onClick={() => setContinued(false)}>
-              <img className="cancelImg" src={cancel} alt="cancel button" />
-            </button>
-          </div>
-          <div className="selection2">
-            <Select
-              value={value}
-              options={toSelect}
-              onChange={handleSelect}
-              placeholder="Select your option"
-              height="4vh"
-              backgroundColor="#C6C6C6"
-            />
-          </div>
-          <div className="form">
-            <p className="errorMessage">{errorMessage}</p>
-            <textarea
-              className="inputArea"
-              placeholder="Message Here"
-              onChange={(e) => setInputMessage(e.target.value)}
-            />
-          </div>
-
-          <button type="button" className="sendButton" onClick={sendMessage}>
-            Send
-          </button>
-          {sent ? (
-            <div className="messageReceived">
-              <button type="button" className="smallCancel" onClick={() => setSent(false)}>
-                <img src={smallCancel} alt="cancel button" />
-              </button>
-              <div className="message">
-                <p>Thank you! Your message has been sent.</p>
-              </div>
+      <div className="wrapper">
+        {!continued ? (
+          <>
+            <div className="noTeamError">
+              <p>{errorMessage}</p>
             </div>
-          ) : (
-            <div />
-          )}
-        </div>
-      )}
+            <div className="contactLanding">
+              <p className="helpQuestion">What do you need help on?</p>
+
+              <div className="selection">
+                <Select
+                  value={value}
+                  options={toSelect}
+                  onChange={handleSelect}
+                  placeholder="Select your option"
+                  height="4vh"
+                  backgroundColor="#C6C6C6"
+                />
+              </div>
+              <button type="button" className="continueButton" onClick={() => continueDecide()}>
+                Continue
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="noInputError">
+              <p>{errorMessage}</p>
+            </div>
+            <div className="newMessage">
+              <div className="topBar">
+                <p>New Message</p>
+                <button
+                  type="button"
+                  className="cancel"
+                  onClick={() => {
+                    setContinued(false);
+                    setErrorMessage("");
+                  }}
+                >
+                  <img className="cancelImg" src={cancel} alt="cancel button" />
+                </button>
+              </div>
+              <div className="selection2">
+                <Select
+                  value={value}
+                  options={toSelect}
+                  onChange={handleSelect}
+                  placeholder="Select your option"
+                  height="4vh"
+                  backgroundColor="#C6C6C6"
+                />
+              </div>
+              <textarea
+                className="inputArea"
+                placeholder="Message Here"
+                onChange={(e) => setInputMessage(e.target.value)}
+              />
+
+              <button type="button" className="sendButton" onClick={sendMessage}>
+                Send
+              </button>
+              {sent ? (
+                <div className="messageReceived">
+                  <button type="button" className="smallCancel" onClick={() => setSent(false)}>
+                    <img src={smallCancel} alt="cancel button" />
+                  </button>
+                  <div className="message">
+                    <p>Thank you! Your message has been sent.</p>
+                  </div>
+                </div>
+              ) : (
+                <div />
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
