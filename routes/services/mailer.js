3861b087c8660643d1a1074cbb30120d0796a603
Jacob Au <jacobau.inbox@gmail.com>
2022-05-09 22:43:40 -0700

    fix mailer

--- routes/services/mailer.js
+++ routes/services/mailer.js
@@ -2,28 +2,47 @@ const nodemailer = require("nodemailer");
 const Email = require("email-templates");
 const config = require("../../config");
 
-const transporter =
-    config.autoEmail.MAIL_USERNAME === ""
-        ? null
-        : nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: config.autoEmail.MAIL_USERNAME,
-        pass: config.autoEmail.MAIL_PASSWORD,
-        clientId: config.autoEmail.CLIENT_ID, // Google Cloud Platform
-        clientSecret: config.autoEmail.CLIENT_SECRET, // Google Cloud Platform
-        refreshToken: config.autoEmail.REFRESH_TOKEN, // OAuth Playground
-    }
+// const transporter =
+//     config.autoEmail.MAIL_USERNAME === ""
+//         ? null
+//         : nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         type: "OAuth2",
+//         user: config.autoEmail.MAIL_USERNAME,
+//         pass: config.autoEmail.MAIL_PASSWORD,
+//         clientId: config.autoEmail.CLIENT_ID, // Google Cloud Platform
+//         clientSecret: config.autoEmail.CLIENT_SECRET, // Google Cloud Platform
+//         refreshToken: config.autoEmail.REFRESH_TOKEN, // OAuth Playground
+//     }
+// });
+
+// const mail =
+//     config.autoEmail.MAIL_USERNAME === ""
+//         ? null
+//         : new Email({
+//               transport: transporter,
+//               send: true,
+//               preview: false,
+//           });
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: config.autoEmail.MAIL_USERNAME,
+    pass: config.autoEmail.MAIL_PASSWORD,
+    clientId: config.autoEmail.CLIENT_ID, // Google Cloud Platform
+    clientSecret: config.autoEmail.CLIENT_SECRET, // Google Cloud Platform
+    refreshToken: config.autoEmail.REFRESH_TOKEN, // OAuth Playground
+  },
 });
 
-const mail =  config.autoEmail.MAIL_USERNAME === ""
-    ? null
-    : new Email({
-        transport: transporter,
-        send: true,
-        preview: false,
-    });
+const mail = new Email({
+  transport: transporter,
+  send: true,
+  preview: false,
+});
 
 /**
  * Populates given email template with locals and sends it to to_email.
@@ -34,29 +53,28 @@ const mail =  config.autoEmail.MAIL_USERNAME === ""
  * @param {string} locals - Information that will populate the email template
  * @returns {object} - Mail object / err
  */
- async function sendEmail(template, to_email, locals) {
-    // sends email only if mail has been successfully setup
-    if (mail != null) {
-        try {
-            await mail.send({
-                template,
-                message: {
-                    from: config.autoEmail.MAIL_USERNAME,
-                    to: to_email,
-                },
-                locals,
-            });
-        } catch (err) {
-            console.error(`Error HERE: Email ${template} could not be sent to ${to_email}. \n${err}`);
-            return false;
-        }
-        return true;
+async function sendEmail(template, to_email, locals) {
+  // sends email only if mail has been successfully setup
+  if (mail != null) {
+    try {
+      await mail.send({
+        template,
+        message: {
+          from: config.autoEmail.MAIL_USERNAME,
+          to: to_email,
+        },
+        locals,
+      });
+    } catch (err) {
+      console.error(`Error HERE: Email ${template} could not be sent to ${to_email}. \n${err}`);
+      return false;
     }
-    console.error(`Error: Email ${template} could not be sent to ${to_email}. Null mailer.`);
-    return false;
+    return true;
+  }
+  console.error(`Error: Email ${template} could not be sent to ${to_email}. Null mailer.`);
+  return false;
 }
 
 module.exports = {
-    sendEmail,
+  sendEmail,
 };
-    
