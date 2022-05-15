0bef08dc1deaf6685c9cc2faa719bb03c9e10ab0
parth4apple <worldrecord00@gmail.com>
2022-05-15 00:49:58 -0700

    put content in stage 4 instead of 5

--- client/src/components/ConfirmMeetGreet.js
+++ client/src/components/ConfirmMeetGreet.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { device } from "../utils/useResponsive";
 import pfp from "../images/pfp.png";
