6f16a120dcdb7788eb2289f471737bb5fed23ff8
Jacob Au <jacobau.inbox@gmail.com>
2022-05-15 15:44:46 -0700

    fix css

--- client/src/components/InterviewInfo.js
+++ client/src/components/InterviewInfo.js
@@ -2,6 +2,7 @@ import { ControlledInput, InputLabel } from "./Input";
 import { useForm } from "react-hook-form";
 import React from "react";
 import Form from "./Form";
+import "../css/interviewInfo.css";
 
 function PassFail({}) {
 
@@ -11,22 +12,30 @@ function PassFail({}) {
 
     const notes = React.useRef();
 
+    const onConfirm = () => {
+
+    }
+
+    const onReject = () => {
+
+    }
+
     return (
         <div className="interview-info-wrapper">
-                {/* <Form.Container>
-                    <Form.Section title="After Interview" ref={notes}> */}
+                <Form.Container>
+                    <Form.Section className="title" title="After Interview" ref={notes}>
                     <ControlledInput
                         control={control}
                         label="Internal Notes"
                         numLines={8}
                         name="fosterInfo.restrictions"
                         />
-                    {/* </Form.Section>
+                    </Form.Section>
                     <Form.Row>
-                        <button type="button" className="reject-button" onClick={onConfirm}>Reject</button>
+                        <button type="button" className="reject-button" onClick={onReject}>Reject</button>
                         <button type="button" className="pass-button" onClick={onConfirm}>Pass</button>
                     </Form.Row>
-                </Form.Container>      */}
+                </Form.Container>     
 
         </div>
     )
