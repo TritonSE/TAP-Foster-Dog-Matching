14d4e835d000898d8beb67e6de0c901e367ca072
artyom2001 <artyom.muradyan2001@gmail.com>
2022-05-31 18:37:44 -0700

    fix files to match main

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
 const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./config");
