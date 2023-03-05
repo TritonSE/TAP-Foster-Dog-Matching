bc08fc561c1408f80d2cd39e41d5557543f4544d
parth4apple <worldrecord00@gmail.com>
2023-03-05 13:02:20 -0800

    admin step 4

--- client/src/pages/Application/AdminView/Step4.js
+++ client/src/pages/Application/AdminView/Step4.js
@@ -2,13 +2,14 @@
  * Application (Admin View) Step 4
  */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FosterMatchingAdmin from "../../../components/FosterMatchingAdmin";
 import DogSelection from "../../../components/DogSelection";
 import LoadingBox from "../../../components/LoadingBox";
 import useInterview from "../../../hooks/useInterview";
 import ApplicationContext from "../../../contexts/ApplicationContext";
 import APPLICATION_STAGES from "../../../constants/APPLICATION_STAGES";
+import { updateApplication } from "../../../services/application";
 
 const VIEW = {
   AVAILABLE_DOGS: "availableDogs",
@@ -17,22 +18,38 @@ const VIEW = {
 };
 
 function AdminFosterMatchingFlow() {
-  const { applicationState } = React.useContext(ApplicationContext);
+  const { applicationId, applicationState } = React.useContext(ApplicationContext);
   const [view, setView] = React.useState(VIEW.AVAILABLE_DOGS);
   const { interview } = useInterview(applicationState.user, APPLICATION_STAGES.HOME_SCREEN);
+  const [selectedDogs, setSelectedDogs] = useState(() => new Set()); // set of ids of admin checked dogs
 
-  React.useEffect(() => {
+  useEffect(() => {
+    console.log(selectedDogs);
+  }, [selectedDogs]);
+
+  useEffect(() => {
     if (interview) setView(VIEW.CONFIRM_MEET_AND_GREET);
     else if (applicationState.selectedDogs.length > 0) setView(VIEW.WAITING);
   }, []);
 
   const handleSelectAvailableDogs = () => {
-    // TODO: handle saving available dog logic here
+    // update dogs admin selected
+    const reqBody = {
+      selectedDogs: Array.from(selectedDogs),
+    };
+    updateApplication(applicationId, reqBody).then((response) =>
+      setApplicationState(response.data.application)
+    );
     setView(VIEW.WAITING);
   };
 
   if (view === VIEW.AVAILABLE_DOGS)
-    return <FosterMatchingAdmin handleConfirm={handleSelectAvailableDogs} />;
+    return (
+      <FosterMatchingAdmin
+        handleConfirm={handleSelectAvailableDogs}
+        selectedDogs={{ selectedDogs, setSelectedDogs }}
+      />
+    );
   if (view === VIEW.WAITING)
     return (
       <LoadingBox
